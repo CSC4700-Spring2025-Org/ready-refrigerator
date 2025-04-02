03c36c48c228307379f8d0695160afe29963b213
Allyssa <166740534+allyssapanganiban@users.noreply.github.com>
2025-04-02 14:11:38 -0400

    Test dev branch

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
-<<<<<<< HEAD
-=======
 import React from 'react';
 import logo from './logo.svg';
->>>>>>> 279c9fa5267ea1bd988b9847b6b37c6413a1983d
 import './App.css';
 import AuthForm from './AuthForm'; //added for firebase
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      
-=======
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
 
         <AuthForm />
       </header>
->>>>>>> 279c9fa5267ea1bd988b9847b6b37c6413a1983d
     </div>
   );
 }
