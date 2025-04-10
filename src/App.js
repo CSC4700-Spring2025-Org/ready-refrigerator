003dc06988f768b29174af7ffc4e47948f70a363
Allyssa <166740534+allyssapanganiban@users.noreply.github.com>
2025-04-09 22:26:44 -0400

    Added inventory dashboard

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import './App.css';
 import AuthForm from './AuthForm'; //added for firebase
+import Inventory from './components/Inventory';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <header className="App-header">
-        <p>
-          Ready Refrigerator
-        </p>
-
-        <AuthForm />
-      </header>
+      <Inventory />
+        {/* <AuthForm /> */}
     </div>
   );
 }
